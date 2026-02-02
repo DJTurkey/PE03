f144477c70b73c23f6b3240d821436b20ca1fb8b
DJTurkey <steven.n.mazanowicz@gmail.com>
2026-02-02 00:01:00 +0000

    i honestly had no idea what to say for the comment

---
+++ tuplesorter.py
@@ -0,0 +1,4 @@
+#Does exactly what was asked. Commenting on 3 lines of code is...well here is me fulfilling a requirement.
+data = [("452", 10), ("256", 5), ("100", 20), ("135", 15)]
+sorted_data = sorted(data, key=lambda item: item[1])
+print(sorted_data)
